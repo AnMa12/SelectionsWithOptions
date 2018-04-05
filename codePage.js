5da3d878528de39d875bc4bcde4b2225328651bc
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-04-05 18:51:32 +0300

    dynamic generation of options
    sliced the code into functions

--- codePage.js
+++ codePage.js
@@ -1,61 +1,51 @@
 window.onload = function () { 
 
-	/* pentru a afisa in select-uri optiunle de selectat 
-	   inseamna ca aceste optiuni trebuie sa fie completate
-	   dinamic, dupa ce valori au mai ramas in tabel*/
 	
-	//var c = document.tr.children;
-    //var txt = "";
-    //var i;
-    //    txt = txt + c[0].innerHTML + "<br>";
-    
-
-    //document.getElementById("demo").innerHTML = txt;
-	var option = document.createElement("option");
-	option.text = "Toate";
-	document.getElementById("selectA").add(option);
+	/* ------ Initializare optiuni select ------ */
 	
-	var option = document.createElement("option");
-	option.text = "Toate";
-	document.getElementById("selectB").add(option);
+		// adaugarea variantei "Toate"
+		optionsAll("selectA"); 
+		optionsAll("selectB"); 
+		optionsAll("selectC"); 
+		
+		/* pentru a afisa in select-uri optiunle de selectat 
+		inseamna ca aceste optiuni trebuie sa fie completate
+		dinamic, dupa ce valori au mai ramas in tabel*/
+		var arrOptions = document.getElementsByTagName("td");
+		
+		for (var i = 0;i < arrOptions.length; i++) {
+			//pentru A, o sa fie pozitiile 0 3 6 9 - conditie i % 3 == 0
+			if (i % 3 == 0) 
+				optionsSet("selectA");
+			//pentru B, o sa fie pozitiile 1 4 7 10 - conditie (i-1) % 3 == 0
+			else if (i % 3 == 1) 
+				optionsSet("selectB");
+			//pentru C, o sa fie pozitiile 2 5 8 11 - conditie (i-2) % 3 == 0
+			else if (i % 3 == 2) 
+				optionsSet("selectC");
+		}		
 	
-	var option = document.createElement("option");
-	option.text = "Toate";
-	document.getElementById("selectC").add(option);	
+	/* ------ --------------------------- ------ */	
 		
 		
 		
-	var arrOptions = document.getElementsByTagName("td");
-	
-	for (var i = 0;i < arrOptions.length; i++){
-		 //pentru A, o sa fie pozitiile 0 3 6 9 - conditie i % 3 == 0
-		 if (i % 3 == 0) {
-			 var option = document.createElement("option");
-			 option.text = arrOptions[i].innerHTML;
-			 document.getElementById("selectA").add(option);
-			 ///!!!!! pentru toate?!?!? add la inceput si luam contor
-			 ///!!!! de adaugat doar daca nu exista 
-		 }
-		 //pentru B, o sa fie pozitiile 1 4 7 10 - conditie (i-1) % 3 == 0
-		 else if (i % 3 == 1) {
-			var option = document.createElement("option");
-			 option.text = arrOptions[i].innerHTML;
-			 document.getElementById("selectB").add(option);
-		 }
-		 else if (i % 3 == 2) {
-			 var option = document.createElement("option");
-			 option.text = arrOptions[i].innerHTML;
-			document.getElementById("selectC").add(option);
-		 }
-		 //pentru C, o sa fie pozitiile 2 5 8 11 - conditie (i-2) % 3 == 0
-	}		
 		
 		
-	function optionsInitialisation 
 		
+	function optionsAll(selectId) {
+		
+		var option = document.createElement("option");
+		option.text = "Toate";
+		document.getElementById(selectId).add(option);	
+		
+	}
 	   
-	function optionsGet(typeX) {
-		
+	function optionsSet(selectId) {
+		var option = document.createElement("option");
+		option.text = arrOptions[i].innerHTML;
+		document.getElementById(selectId).add(option);
+		///!!!!! pentru toate?!?!? add la inceput si luam contor
+        ///!!!! de adaugat doar daca nu exista 
 	}
 
 
