ef1d2e2f3a07a29a2c76ca7099f7a17bc0a58134
Ana-Maria Melinte <34279579+AnMa12@users.noreply.github.com>
2018-04-10 00:28:12 +0300

    finish change selectA

--- codePage.js
+++ codePage.js
@@ -5,15 +5,17 @@ window.onload = function () {
 		optionsAll("selectA"); 
 		optionsAll("selectB"); 
 		optionsAll("selectC"); 
-		optionsGet();
+		optionsDynamic();
 	/* ------ Initializare optiuni select FIN ------ */	
 	
 	/* ---- Filter the options ---- */
-	
-	//filter();
-	
-	document.getElementById("selectA").onchange = function () {
-		var arrSelections = document.getElementById("selectA");
+	document.getElementById("selectA").setAttribute("onchange","optionChanged('selectA','selectB','selectC')");
+	document.getElementById("selectB").setAttribute("onchange","optionChanged('selectB','selectA','selectC')");
+	document.getElementById("selectC").setAttribute("onchange","optionChanged('selectC','selectB','selectA')");
+	/* ---- Filter the options FIN ---- */
+}
+function optionChanged(currentSelectId, secondSelectId, thirdSelectId) {
+	var arrSelections = document.getElementById(currentSelectId);
 			for(var i = 0; i < arrSelections.length; i++)
 				if(arrSelections[i].selected == true) 
 					var input = arrSelections[i].innerHTML;
@@ -27,12 +29,21 @@ window.onload = function () {
 						var options = tr[i].getElementsByTagName("td");
 						for (var j = 0; j < 3; j++) 
 							options[j].style.display = "none";
-					}	
+					} else {
+						tr[i].style.display = "";
+						var options = tr[i].getElementsByTagName("td");
+						for (var j = 0; j < 3; j++) 
+							options[j].style.display = "";
+					}
 				}
-		
-		$("#selectB").empty();
-		$("#selectC").empty();
-		
+		$("#" + secondSelectId).empty();
+		$("#" + thirdSelectId).empty();
+		optionsDynamic();
+}
+
+function optionsDynamic() {
+	/* pentru a afisa in select-uri optiunle de selectat inseamna ca aceste 
+		optiuni trebuie sa fie completate dinamic, dupa ce valori au mai ramas in tabel*/
 		var arrOptions = document.getElementsByTagName("td");
 		for (var i = 0; i < arrOptions.length; i++) {
 			if(arrOptions[i].style.display !== "none") {
@@ -45,51 +56,8 @@ window.onload = function () {
 			//pentru C, sunt pozitiile 2 5 8 11 - conditie (i-2) % 3 == 0
 			else if (i % 3 == 2) 
 				optionsSet("selectC", arrOptions, i);
-			alert("*" + arrOptions[i].innerHTML + "*");
 			}
 		}	
-	}
-		
-		
-	
-	/* ---- Filter the options FIN ---- */
-
-	
-	
-	
-	/*function filter() {
-		table = document.getElementById("myTable");
-			tr = table.getElementsByTagName("tr");
-				for (i = 0; i < tr.length; i++) {
-					td = tr[i].getElementsByTagName("td")[0];
-					if (td) {
-						if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-							tr[i].style.display = "";
-					} else {
-					
-					}
-			}       
-		}
-	}*/
-
-}
-
-function optionsGet() {
-	/* pentru a afisa in select-uri optiunle de selectat inseamna ca aceste 
-		optiuni trebuie sa fie completate dinamic, dupa ce valori au mai ramas in tabel*/
-		var arrOptions = document.getElementsByTagName("td");
-		
-		for (var i = 0;i < arrOptions.length; i++) {
-			//pentru A, sunt pozitiile 0 3 6 9 - conditie i % 3 == 0
-			if (i % 3 == 0) 
-				optionsSet("selectA", arrOptions, i);
-			//pentru B, sunt pozitiile 1 4 7 10 - conditie (i-1) % 3 == 0
-			else if (i % 3 == 1) 
-				optionsSet("selectB", arrOptions, i);
-			//pentru C, sunt pozitiile 2 5 8 11 - conditie (i-2) % 3 == 0
-			else if (i % 3 == 2) 
-				optionsSet("selectC", arrOptions, i);
-		}
 }
 
 function optionsSet(selectId, arrOptions, i) {
